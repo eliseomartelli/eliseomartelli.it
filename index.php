beb1e2a094fddea127b1b9027d77529072559640
eliseomartelli <martely98@gmail.com>
2017-03-27 19:15:35 +0200

    new design

--- index.php
+++ index.php
@@ -3,38 +3,58 @@
     <head>
         <meta charset="utf-8">
         <title>Eliseo Martelli</title>
-        <meta name="theme-color" content="#2196f3">
-        <link rel="stylesheet" href="res/style.php" media="screen" title="no title">
-        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="https://use.fontawesome.com/a62165eaa4.js"></script>
+        <link rel="stylesheet" href="res/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#ffffff">
+        <script src="https://use.fontawesome.com/a62165eaa4.js"></script><link href="https://use.fontawesome.com/a62165eaa4.css" media="all" rel="stylesheet">
     </head>
     <body>
-        <header>
-          <div class="hero"></div>
+        <div class="full-width container toolbar center-content">
+            <div class="logo">
+                eliseo<span>martelli</span>
+            </div>
+        </div>
+        <header class="full-width container center-content">
+
+                <p class="desc grid">
+                    I make, break and fix things.
+                </p>
+
         </header>
         <main>
-            <div id="main-container" class="container">
-
-                <img class="profile-pic" src="https://pbs.twimg.com/profile_images/802815721306816512/ulg-IvVK.jpg" alt="Eliseo Martelli" />
-                <div class="card">
-                    <p id="name">
-                        Eliseo Martelli
-                    </p>
-                    <p id="job">
-                        Android and Web developer
-                    </p>
-                    <p  id="email">
-                        <a class="icon-a" href="mailto:me@eliseomartelli.it">
-                            me@eliseomartelli.it
-                        </a>
-                    </p>
+            <div class="container full-width center-content section">
+                <div class="grid">
+                    <img class="profile container" src="res/profile.jpg" alt="">
+                    <p>I’m Eliseo Martelli.<br>I’m an Android and Web developer but I’m also into creative stuffs.</p>
+                </div>
+            </div>
+            <div class="container full-width center-content section section-bis">
+                <h2>Check out my projects</h2>
+                <div class="grid">
+                    <div class="card container">
+                        <h4>Citazioni</h4>
+                        <img src="res/appicons/citazioni.png" alt=""><br>
+                        <a href='https://play.google.com/store/apps/details?id=eliseo.citazioni&hl=it&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+                    </div>
+                    <div class="card container">
+                        <h4>NightMode</h4>
+                        <img src="res/appicons/nightmode.png" alt=""><br>
+                        <a href='https://play.google.com/store/apps/details?id=eliseo.nightmode&hl=it&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+                    </div>
+                    <div class="card container">
+                        <h4>WatchStatus</h4>
+                        <img src="res/appicons/watchstatus.png" alt=""><br>
+                        <a href='https://play.google.com/store/apps/details?id=it.eliseomartelli.watchstatus&hl=it&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+                    </div>
+                </div>
+            </div>
+            <div class="container full-width center-content section">
+                <h2>Get in touch</h2>
+                <p style="font-weight: 300;">I love connecting with people :)</p>
+                <div class="grid">
                     <p id="social">
                         <a class="icon-a" href="https://www.facebook.com/eliseomartelli" target="_blank">
                             <i class="fa fa-2x fa-facebook-official" aria-hidden="true"></i>
-                        </a>
-                        <a class="icon-a" href="https://twitter.com/EliseoMartelli" target="_blank">
-                            <i class="fa fa-2x fa-twitter" aria-hidden="true"></i>
                         </a>
                         <a class="icon-a" href="https://plus.google.com/u/0/+EliseoMartelliMZG" target="_blank">
                             <i class="fa fa-2x fa-google-plus-official" aria-hidden="true"></i>
@@ -45,21 +65,24 @@
                         <a class="icon-a" href="https://www.snapchat.com/add/eliseomartelli/" target="_blank">
                             <i class="fa fa-2x fa-snapchat-ghost" aria-hidden="true"></i>
                         </a>
-                        <a class="icon-a" href="https://t.me/EliseoMartelli" target="_blank">
-                            <i class="fa fa-2x fa-telegram" aria-hidden="true"></i>
+                        <a class="icon-a" href="https://twitter.com/EliseoMartelli" target="_blank">
+                            <i class="fa fa-2x fa-twitter" aria-hidden="true"></i>
                         </a>
                         <a class="icon-a" href="https://github.com/eliseomartelli" target="_blank">
                             <i class="fa fa-2x fa-github" aria-hidden="true"></i>
                         </a>
-
-
+                        <a class="icon-a" href="http://codepen.io/eliseomartelli/" target="_blank">
+                            <i class="fa fa-2x fa-codepen" aria-hidden="true"></i>
+                        </a>
                     </p>
-
+                    <p class="or">- or -</p>
+                    <a class="icon-a email" href="mailto:me@eliseomartelli.it">me@eliseomartelli.it</a>
                 </div>
+
             </div>
         </main>
         <footer>
-            Site made with love by me
+
         </footer>
     </body>
 </html>
