5df60a5a1d02d9d43c61c975d2465c6fbbe774c1
eliseomartelli <martely98@gmail.com>
2017-03-28 14:17:10 +0200

    modularized

---
+++ res/layouts/home-presentation.php
@@ -0,0 +1,6 @@
+<div class="full-width center-content section">
+    <div class="grid">
+        <img class="profile container" src="res/profile.jpg" alt="">
+        <p>Hi! It's Eliseo Martelli.<br>I’m an Android and Web developer but I’m also into creative stuffs.</p>
+    </div>
+</div>
---
+++ res/layouts/toolbar.php
@@ -0,0 +1,5 @@
+<div class="full-width container toolbar center-content">
+    <div class="logo">
+        eliseo<span>martelli</span>
+    </div>
+</div>
