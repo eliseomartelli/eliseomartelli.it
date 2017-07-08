564f0251ea8a6281694dcfd7244a8467c029f5d0
eliseomartelli <martely98@gmail.com>
2017-07-08 09:42:25 +0200

    Added donate button

---
+++ res/layouts/home-donate.php
@@ -0,0 +1,13 @@
+<?php
+$socials = json_decode(file_get_contents("https://cdn.rawgit.com/eliseomartelli/personal-site__data/master/social-links.json"));
+?>
+<div class="full-width center-content section section-bis">
+    <h2>Do you like my work?</h2>
+    <p style="font-weight: 300;">You can donate to support development</p>
+    <div class="grid">
+        <a href="http://paypal.me/eliseomartelli">
+            <img src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/blue-pill-paypal-60px.png" alt="PayPal">
+        </a>
+    </div>
+
+</div>
