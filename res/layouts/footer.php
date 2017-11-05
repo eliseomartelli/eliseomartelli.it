9a18f57a1657d4c5b7fc25f07ca79e0d36d44359
Eliseo Martelli <martely98@gmail.com>
2017-11-05 12:54:05 +0100

    Staerted V2

--- res/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="full-width center-content">
-        <div class="grid">
-            <p>Site made with love by <span class="logo">
-                eliseo<span>martelli</span>
-            </span></p>
-        </div>
-    </div>
-</footer>
