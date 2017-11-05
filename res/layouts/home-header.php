3ddfbd36c4b3e8fa92271856a19e7c036039c6bb
Eliseo Martelli <martely98@gmail.com>
2017-11-05 12:54:05 +0100

    Staerted V2

--- res/layouts/home-header.php
+++
@@ -1,5 +0,0 @@
-<header class="full-width container center-content">
-    <p class="desc grid">
-        I make, break and fix things.
-    </p>
-</header>
--- res/layouts/toolbar.php
+++
@@ -1,5 +0,0 @@
-<div class="full-width container toolbar center-content">
-    <div class="logo">
-        eliseo<span>martelli</span>
-    </div>
-</div>
