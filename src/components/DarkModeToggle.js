0cbdf5763ccb1dea3ebf8e89af38a74f793b57da
Eliseo Martelli <me@eliseomartelli.it>
2020-10-21 19:52:33 +0200

    feat: add theme context

---
+++ src/components/DarkModeToggle.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import ThemeContext from "../context/ThemeContext"
+
+export default class DarkModeToggle extends Component {
+    render() {
+        return (
+            <ThemeContext.Consumer>
+                {theme => (
+                    <button onClick={theme.toggleDarkTheme}>{theme.darkTheme ? "Light" : "Dark"}</button>
+                )}
+            </ThemeContext.Consumer>
+        )
+    }
+}
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const defaultState = {
+    darkTheme: false,
+    toggleDarkTheme: () => {}
+};
+
+const ThemeContext = React.createContext(defaultState);
+
+const isSystemDarkMode = () =>  (
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+);
+
+class ThemeProvider extends React.Component {
+    state = {
+        darkTheme: false,
+    }
+
+    toggleDarkTheme = () => {
+        let newState = !this.state.darkTheme;
+        localStorage.setItem('darkTheme', JSON.stringify(newState));
+        this.setState({darkTheme: newState});
+    }
+
+    componentDidMount() {
+        const isDarkTheme = JSON.parse(localStorage.getItem('darkTheme'));
+        if (isDarkTheme !== null) {
+            this.setState({darkTheme: isDarkTheme});
+        } else if (isSystemDarkMode()) {
+            this.setState({darkTheme: true});
+        }
+    }
+
+    render() {
+        const { children } = this.props;
+        const { darkTheme } = this.state;
+        return (
+            <ThemeContext.Provider
+                value={{darkTheme, toggleDarkTheme: this.toggleDarkTheme}}>
+                 {children}
+            </ThemeContext.Provider>
+        )
+    }
+}
+
+export default ThemeContext;
+export {ThemeProvider};
