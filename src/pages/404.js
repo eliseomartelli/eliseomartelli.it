eaf3614ac8ffdb43dbd99d4c5b01d671260f4c67
Eliseo Martelli <me@eliseomartelli.it>
2020-10-23 21:51:02 +0200

    fead: add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+import App from "../components/App"
+import { Browser } from "react-kawaii"
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <App>
+        <div
+          style={{
+            display: "flex",
+            height: "45vh",
+            flexDirection: "column",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: 128,
+          }}
+        >
+          <h1>404: Not Found</h1>
+          <Browser size={150} mood="sad" color="var(--primary)" />
+          <p>The page you are looking for does not exist.</p>
+        </div>
+      </App>
+    )
+  }
+}
