f164f60f3633308dddc8c113291f29649953a5e0
Eliseo Martelli <me@eliseomartelli.it>
2020-10-23 18:16:42 +0200

    fix: color variables from typography

--- src/utils/Typography.js
+++ src/utils/Typography.js
@@ -1,6 +1,25 @@
 import Typography from "typography"
 import grandViewTheme from "typography-theme-grand-view"
 
+grandViewTheme.overrideThemeStyles = () => ({
+  a: {
+    color: "var(--secondary)",
+  },
+  "a:hover": { color: "var(--primary)" },
+  blockquote: {
+    color: "inherit",
+    borderLeftColor: "var(--primary)",
+  },
+  // These two are for gatsby-remark-autolink-headers:
+  "a.anchor": {
+    boxShadow: "none",
+  },
+  'a.anchor svg[aria-hidden="true"]': {
+    stroke: "var(--secondary)",
+  },
+  "h1, h2, h3, h4, h5": { color: "var(--text)" },
+})
+
 const typography = new Typography(grandViewTheme)
 
 export const { scale, rhythm, options } = typography
