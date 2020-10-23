6f1e31a77809213532a523afbf8f6fe3f4d75627
Eliseo Martelli <me@eliseomartelli.it>
2020-10-23 18:16:05 +0200

    fix: color variables from typography

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        --primary: ${props => props.theme.primary};
+        --secondary: ${props => props.theme.secondary};
+        --text: ${props => props.theme.text};
+        --background: ${props => props.theme.background};
+        --secondaryBackground: ${props => props.theme.secondaryBackground};
+        
+    }
     body {
         margin: 0;
         background: ${props => props.theme.background};
