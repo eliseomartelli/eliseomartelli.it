9c0134073cd04402cc768d76fa7f1d996bd6570b
Eliseo Martelli <me@eliseomartelli.it>
2020-10-24 11:16:59 +0200

    fix: changed color palette

--- src/Themes.js
+++ src/Themes.js
@@ -1,14 +1,14 @@
 export const Themes = {
   dark: {
-    primary: "#FF384D",
-    secondary: "#EB001A",
+    primary: "#DC267F",
+    secondary: "#df6995",
     text: "#FFFFFF",
     background: "#171117",
     secondaryBackground: "#221C24",
   },
   light: {
-    primary: "#457B85",
-    secondary: "#13a89c",
+    primary: "#648FFF",
+    secondary: "#785EF0",
     text: "#000000",
     background: "#fffefd",
     secondaryBackground: "#FAFAFA",
