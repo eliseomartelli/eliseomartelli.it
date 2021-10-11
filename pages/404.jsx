1b63d06a8be727be0dd145cbbce7835b19572608
Eliseo Martelli <me@eliseomartelli.it>
2021-10-11 14:00:11 +0200

    feat: add 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="prose text-center w-full">
+      <h1>🔎 Not found</h1>
+      <p>Uh oh! The page you're looking for is nowhere to be found.</p>
+    </div>
+  );
+};
+
+export default NotFound;
