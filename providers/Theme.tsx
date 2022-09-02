dfebfa50ba62e129ba8ac967c1443f909664e0fa
Eliseo Martelli <me@eliseomartelli.it>
2022-09-02 10:40:10 +0200

    feat: store theme in localStorage

--- providers/Theme.tsx
+++ providers/Theme.tsx
@@ -1,17 +1,47 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 interface ThemeContextType {
-  darkMode?: boolean;
-  toggleDarkMode?: Function;
+  darkMode: boolean | null;
+  toggleDarkMode: Function;
 }
 
-export const ThemeContext = createContext<ThemeContextType>({});
+export const ThemeContext = createContext<ThemeContextType>({
+  darkMode: false,
+  toggleDarkMode: () => {},
+});
+
+const LSDarkModeKey = "darkMode";
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const [darkMode, setDarkMode] = useState(false);
-  const toggleDarkMode = () => {
+  const [darkMode, setDarkMode] = useState<boolean | null>(null);
+
+  function toggleDarkMode() {
     setDarkMode(!darkMode);
-  };
+  }
+
+  function getInitialState(): boolean {
+    const darkModeLS = localStorage.getItem(LSDarkModeKey);
+    if (darkModeLS !== null) {
+      const isDarkModeStored = JSON.parse(darkModeLS);
+      return isDarkModeStored;
+    } else {
+      return isSystemDarkMode();
+    }
+  }
+
+  function isSystemDarkMode() {
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  }
+
+  useEffect(() => {
+    setDarkMode(getInitialState());
+  }, []);
+
+  useEffect(() => {
+    if (darkMode !== null)
+      localStorage.setItem(LSDarkModeKey, JSON.stringify(darkMode));
+  }, [darkMode]);
+
   return (
     <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
