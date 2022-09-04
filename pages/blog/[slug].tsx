dcb4a911849c91b79504326ffca4e5d4bb509324
Eliseo Martelli <me@eliseomartelli.it>
2022-09-04 13:53:58 +0200

    fix: center image in prose

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -30,7 +30,7 @@ export default function BlogPost({ content }: BlogPostProps): JSX.Element {
         <p className="mb-2">{content.frontmatter?.date}</p>
         <h1 className="text-3xl font-bold">{content.frontmatter?.title}</h1>
       </div>
-      <article className="prose mx-auto w-full max-w-full">
+      <article className="prose mx-auto w-full max-w-full prose-img:mx-auto">
         <MDXRemote {...content} components={{ Newsletter }} />
       </article>
       <Button
