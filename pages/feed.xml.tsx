99a44604344b589508a9a0787a4ab323e367e58b
Eliseo Martelli <me@eliseomartelli.it>
2022-09-10 10:59:05 +0200

    feat: add full content to feed

--- pages/feed.xml.tsx
+++ pages/feed.xml.tsx
@@ -2,6 +2,11 @@ import RSS from "rss";
 
 import { GetServerSidePropsContext } from "next";
 import { loadPosts } from "../lib/posts";
+import path from "path";
+import fs from "fs";
+import { MDXRemote } from "next-mdx-remote";
+import { mdxToHTML } from "../lib/mdx";
+import { renderToString } from "react-dom/server";
 
 export async function getServerSideProps({ res }: GetServerSidePropsContext) {
   const feed = new RSS({
@@ -10,15 +15,26 @@ export async function getServerSideProps({ res }: GetServerSidePropsContext) {
     feed_url: "https://eliseomartelli.it/feed.xml",
   });
 
-  const posts = loadPosts();
-  posts.forEach((post) => {
-    feed.item({
-      title: post.frontmatter.title,
-      url: `https://eliseomartelli.it/blog/${post.slug}`,
-      date: post.frontmatter.date,
-      description: post.frontmatter.excerpt!,
-    });
-  });
+  const posts = loadPosts().splice(0, 5); // Get latest 5 posts only.
+
+  await Promise.all(
+    posts.map(async (post) => {
+      post.slug += ".md";
+      const filePath = path.join("posts", post.slug);
+      const file = fs.readFileSync(filePath, "utf-8");
+
+      const { html } = await mdxToHTML(file);
+
+      const content = renderToString(<MDXRemote {...html} />);
+
+      feed.item({
+        title: post.frontmatter.title,
+        url: `https://eliseomartelli.it/blog/${post.slug}`,
+        date: post.frontmatter.date,
+        description: content,
+      });
+    })
+  );
 
   res.setHeader("Content-Type", "text/xml");
   res.setHeader(
