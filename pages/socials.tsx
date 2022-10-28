1d9e3d52eb6361707f360fb3169732f417141575
Eliseo Martelli <me@eliseomartelli.it>
2022-10-28 19:39:11 +0200

    style: fix function export

--- pages/socials.tsx
+++ pages/socials.tsx
@@ -1,15 +1,15 @@
-import Container from "../components/Container.tsx";
-import { socials } from "../socials.ts";
+import Container from "../components/Container";
+import { socials } from "../socials";
 
-export default function () {
+export default function Socials(): JSX.Element {
   return (
     <Container>
-      <div class="prose">
+      <div className="prose">
         <h1>Socials</h1>
         <p>
           Here are some social media websites (and an email) you can use to keep
-          in touch with me. I'll use the appropriate platform for the media I'm
-          sharing.
+          in touch with me. I&apos;ll use the appropriate platform for the media
+          I&apos;m sharing.
         </p>
         <ul>
           {socials.map((e, i) => (
