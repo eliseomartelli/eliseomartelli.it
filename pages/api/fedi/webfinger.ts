75e3d3cd4136d6ddf1f1480e544191989ca26b48
Eliseo Martelli <me@eliseomartelli.it>
2022-11-09 14:15:31 +0100

    feat: mvp fediverse

---
+++ pages/api/fedi/webfinger.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type WebfingerType = {
+  subject: string;
+  links: [
+    {
+      rel: "self";
+      type: "application/activity+json";
+      href: string;
+    }
+  ];
+};
+
+export default async function handler(
+  _: NextApiRequest,
+  res: NextApiResponse<WebfingerType>
+) {
+  const { ACTIVITYPUB_DOMAIN } = process.env;
+  res.status(200).json({
+    subject: "site",
+    links: [
+      {
+        rel: "self",
+        type: "application/activity+json",
+        href: `https://${ACTIVITYPUB_DOMAIN}/actor`,
+      },
+    ],
+  });
+}
