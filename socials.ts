80c832d09dd2a80d6661eddbc80e6475a13edf5b
Eliseo Martelli <me@eliseomartelli.it>
2022-11-19 11:52:52 +0100

    feat: add keyoxide

--- socials.ts
+++ socials.ts
@@ -5,14 +5,8 @@ type Social = {
 };
 
 export const socials: Social[] = [
-  {
-    name: "Email: me@eliseomartelli.it",
-    url: "mailto:me@eliseomartelli.it",
-  },
-  {
-    name: "GitHub: @eliseomartelli",
-    url: "https://github.com/eliseomartelli",
-  },
+  { name: "Email: me@eliseomartelli.it", url: "mailto:me@eliseomartelli.it" },
+  { name: "GitHub: @eliseomartelli", url: "https://github.com/eliseomartelli" },
   {
     name: "Instagram: @eliseomartelli",
     url: "https://instagram.com/eliseomartelli",
@@ -22,12 +16,13 @@ export const socials: Social[] = [
     url: "https://mastodon.social/@eliseomartelli",
     customRel: "me",
   },
-  {
-    name: "Telegram: @eliseomartelli",
-    url: "https://t.me/eliseomartelli",
-  },
+  { name: "Telegram: @eliseomartelli", url: "https://t.me/eliseomartelli" },
   {
     name: "Twitter: @eliseomartelli",
     url: "https://twitter.com/eliseomartelli",
+  },
+  {
+    name: "Keyoxide: fe81cdb04848cc6e29f85a499560e7af90f0e1b9",
+    url: "https://keyoxide.org/me@eliseomartelli.it",
   },
 ];
