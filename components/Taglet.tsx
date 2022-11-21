b5e4c98f6c1cb86e0d9b5ea500043045952fddc8
Eliseo Martelli <me@eliseomartelli.it>
2022-11-21 12:31:07 +0100

    fix: improve accessibility

--- components/Taglet.tsx
+++ components/Taglet.tsx
@@ -5,18 +5,21 @@ const Color: { [name: string]: string } = {
   network: "bg-pink-50 border-pink-300",
   iot: "bg-blue-50 border-blue-300",
   ansible: "bg-red-50 border-red-300",
+  short: "bg-orange-50 border-orange-300",
   automation: "bg-green-50 border-green-300",
   default: "bg-gray-50 border-gray-300",
 };
 
 export const Taglet = ({ tag }: { tag: string }) => (
-  <Link href={`/blog/category/${tag}`}>
-    <span
-      className={`px-2.5 py-0.5 border ${
-        Color[tag.toLowerCase()] || Color.default
-      } rounded-full font-medium text-xs cursor-pointer`}
-    >
-      {tag}
-    </span>
+  <Link href={`/blog/category/${tag}`} passHref>
+    <a>
+      <span
+        className={`px-2.5 py-0.5 border ${
+          Color[tag.toLowerCase()] || Color.default
+        } rounded-full font-medium text-xs cursor-pointer`}
+      >
+        {tag}
+      </span>
+    </a>
   </Link>
 );
