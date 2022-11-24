3ab1e8a2b1982a8ca7c01463d14b23edbb97853d
Eliseo Martelli <me@eliseomartelli.it>
2022-11-24 15:26:32 +0100

    feat: add photo page

---
+++ pages/photos.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function photos() {
+  return (
+    <div className="flex">
+      <aside>
+        <h1>Journeys</h1>
+        <p className="font-bold italic">noun</p>
+        <ol className="list-decimal ml-8">
+          <li>the act of travelling from one place to another;</li>
+          <li>listening to our artistic voice.</li>
+        </ol>
+      </aside>
+      <main className="grow"></main>
+    </div>
+  );
+}
