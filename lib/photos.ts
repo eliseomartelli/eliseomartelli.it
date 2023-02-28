ef39feae68a5cfdf423c4e4122e70aef08bc5f61
Eliseo Martelli <me@eliseomartelli.it>
2023-02-28 08:53:43 +0100

    fix: remove random sorting

--- lib/photos.ts
+++ lib/photos.ts
@@ -1,7 +1,5 @@
 import fs from "fs";
 
 export function loadPhotoFiles() {
-  return fs
-    .readdirSync("public/image_portfolio")
-    .sort(() => 0.5 - Math.random());
+  return fs.readdirSync("public/image_portfolio");
 }
