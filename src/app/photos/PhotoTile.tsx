6e184e9d04fb030a15f736f91e66431428a70e3f
Eliseo Martelli <me@eliseomartelli.it>
2023-04-15 21:08:13 +0200

    feat: add proto-photo  viewing

--- src/app/photos/PhotoTile.tsx
+++ src/app/photos/PhotoTile.tsx
@@ -2,6 +2,7 @@
 
 import { Photo } from "contentlayer/generated";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const PhotoComponent = ({ url, title, aspect }: Photo) => {
@@ -12,22 +13,24 @@ const PhotoComponent = ({ url, title, aspect }: Photo) => {
         aspect == "vertical" ? "aspect-vertical" : "aspect-horizontal"
       } bg-gray-400 ${
         loading && "animate-pulse"
-      } rounded-md overflow-hidden mb-4 z-0`}
+      } rounded-md overflow-hidden z-0 mb-4`}
     >
-      <Image
-        src={url!}
-        fill
-        loading="lazy"
-        alt={title!}
-        sizes="
+      <Link href={url!}>
+        <Image
+          src={url!}
+          fill
+          loading="lazy"
+          alt={title!}
+          sizes="
               (min-width: 640px) 50vw,
               (min-width: 768px) 33vw,
               25vw"
-        className="!relative "
-        onLoad={() => {
-          setLoading(false);
-        }}
-      />
+          className="!relative "
+          onLoad={() => {
+            setLoading(false);
+          }}
+        />
+      </Link>
     </div>
   );
 };
--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -13,18 +13,20 @@ const Photos = () => {
       <WidthLimit>
         <h1 className="text-4xl font-bold mb-4">Photos</h1>
       </WidthLimit>
-      {allPhotos.map((photoSection, i) => (
-        <section key={i}>
-          <h2 className="text-2xl font-bold text-center">
-            {photoSection.title}
-          </h2>
-          <div className="gap-4 columns-2  md:columns-3 lg:columns-4 p-4">
-            {photoSection.photos?.map((photo, i) => (
-              <PhotoComponent {...photo} key={i} />
-            ))}
-          </div>
-        </section>
-      ))}
+      <div className="flex flex-col gap-8">
+        {allPhotos.map((photoSection, i) => (
+          <section key={i}>
+            <h2 className="text-2xl font-bold text-center">
+              {photoSection.title}
+            </h2>
+            <div className="columns-2 md:columns-3 lg:columns-4 p-4">
+              {photoSection.photos?.map((photo, i) => (
+                <PhotoComponent {...photo} key={i} />
+              ))}
+            </div>
+          </section>
+        ))}
+      </div>
     </>
   );
 };
