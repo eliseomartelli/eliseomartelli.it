5b1788549610d7e51591e5d9d04a965312d38366
Eliseo Martelli <me@eliseomartelli.it>
2024-10-14 12:21:07 +0200

    feat: rework landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import { Menu } from "@/components/Icons";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
-import { DefaultNavbar } from "@/components/Navbar";
+import Link from "next/link";
 
 export const metadata = {
   title: "Eliseo Martelli",
@@ -19,12 +18,34 @@ export default function RootLayout({
       lang="en"
       className="min-h-screen scroll-smooth bg-stone-50 text-stone-950"
     >
-      <body className="flex flex-col w-full min-h-screen">
+      <body className="flex flex-col w-full">
         <Analytics />
-        <DefaultNavbar />
+        <Navbar />
         <main className="flex flex-col min-h-screen">{children}</main>
         <Footer />
       </body>
     </html>
   );
+}
+
+const Navbar = () =>
+  <div className="p-4 z-50 fixed top-0 left-0 bg-stone-50 w-full flex flex-row justify-between">
+    <HomeLogo />
+    <ul className="flex gap-4">
+      <li>Blog</li>
+      <li>Photos</li>
+      <li>About</li>
+    </ul>
+  </div>;
+function HomeLogo() {
+  return <Link href="/"><div className="font-mono text-rose-600 font-bold text-xl">
+    <div className="group relative h-8 w-48 overflow-y-hidden flex items-center">
+      <div className="absolute transition-transform duration-200 transform translate-y-0 group-hover:-translate-y-full">
+        <p>EM</p>
+      </div>
+      <div className="absolute transition-transform duration-200 transform translate-y-full group-hover:translate-y-0">
+        <p>eliseomartelli</p>
+      </div>
+    </div>
+  </div></Link>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,91 @@
-import { PageLayout } from "@/components/PageLayout";
-import WidthLimit from "@/components/WidthLimit";
+import moo from "@eliseomartelli/moo/dist";
 import Link from "next/link";
+import { ReactNode } from "react";
+import { AspectRatioImage } from "./wallpapers/AspectRatioImage";
+import Button, { Color, getButtonClassNames } from "@/components/Button";
 
 export const metadata = { title: "Home - Eliseo Martelli" };
 
+interface IHomeSection {
+  children: JSX.Element | ReactNode;
+  className?: string;
+}
+
+const HomeSection = ({ children, className }: IHomeSection) =>
+  <section className={moo("min-h-screen relative", className!)}>{children}</section>
+
+interface IHomeSectionTitle {
+  children: JSX.Element | ReactNode;
+  className?: string;
+}
+
+const HomeSectionTitle = ({ children, className }: IHomeSectionTitle) =>
+  <h1 className={moo(
+    "text-6xl font-bold font-serif italic mix-blend-difference text-white max-w-md py-2",
+    className!)}>{children}</h1>
+
 const Home = () => {
   return (
-    <PageLayout>
-      <WidthLimit className="prose mx-auto">
-        <p>
-          I’m a software developer and visual artist based in Turin, balancing my
-          passion for technology and creativity.
-          As a community builder, I enjoy collaboration and sharing knowledge
-          within my networks.
-          My current focus is on developing a reproducible computing platform for
-          bioinformatics research.
-        </p>
-        <p>
-          Outside of work, I’m interested in art, photography, and music.<br />
-          I value growth and enjoy contributing to the communities I’m part of,
-          both locally and globally.
-        </p>
-        <Link href={"/about"}>about</Link>{" "}
-        <Link
-          href={
-            "http://github.com/eliseomartelli/cv/releases/latest/download/cv.pdf"
-          }
-        >
-          cv
-        </Link>
-      </WidthLimit>
-    </PageLayout >
+    <div className="p-4 flex flex-col gap-4">
+      <HomeSection>
+        <HomeSectionTitle className="self-end sticky top-1/2 z-10 ms-auto text-end">
+          Visual Arts
+        </HomeSectionTitle>
+        <div className="w-full flex flex-col gap-4">
+          <div className="w-1/3 self-center">
+            <AspectRatioImage src="/photoPortfolio/street/IMG_0040.jpg" alt="Photography of condensation" />
+          </div>
+          <div className="w-1/3 ms-auto">
+            <AspectRatioImage src="/photoPortfolio/street/IMG_6356.jpg" alt="Photography of condensation" />
+          </div>
+          <div className="w-2/3 self-start">
+            <AspectRatioImage src="/photoPortfolio/concerts/IMG_0461.jpg" alt="Photography of condensation" />
+          </div>
+        </div>
+      </HomeSection>
+      <HomeSection className="bg-emerald-800 -mx-4 px-4">
+        <HomeSectionTitle className="self-end sticky top-1/2 z-10">
+          Software Development
+        </HomeSectionTitle>
+        <div className="flex flex-col gap-4 prose p-4 font-serif max-w-prose ms-auto w-full prose-xl text-white prose-a:text-pink-300">
+          <p>
+            My work focuses on building robust systems that support
+            bioinformatics research. Currently, I'm developing a reproducible
+            computing platform aimed at advancing bioinformatics by ensuring
+            that research workflows are consistent, scalable, and efficient.
+          </p>
+          <p>
+            Download my{" "}
+            <Link href="http://github.com/eliseomartelli/cv/releases/latest/download/cv.pdf">
+              CV
+            </Link >
+            {" "}or have a look at my{" "}
+            <Link href="/about">
+              about page.
+            </Link >
+          </p>
+        </div>
+      </HomeSection>
+      <HomeSection className="items-end not-prose">
+        <div className="sticky mt-auto mix-blend-difference font-serif text-white top-1/2 z-10 text-end py-2 leading-[0.99]">
+          <p className="text-[16vw]">Eliseo Martelli</p>
+          <div className="prose ml-auto text-white prose-xl">
+            <p className="text-end">(this is me)</p>
+            <p>
+              P.S.: I love handcrafting human experiences and building communities.
+            </p>
+            <div className="pt-4">
+              <Link href="/contact" className={getButtonClassNames({ color: Color.Red, className: "mt-8", noRounded: true })}>
+                Get in touch
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="w-2/3 self-end">
+          <AspectRatioImage src="/profilePicture/colour.jpg" alt="Photography of condensation" />
+        </div>
+      </HomeSection>
+    </div>
   );
 };
 
