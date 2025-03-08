3e8bcaebb6f1fb98e4725e0055e2ca7d770e3ae7
Eliseo Martelli <me@eliseomartelli.it>
2025-03-08 20:05:25 +0100

    fix(contentlayer.config.js): prepend slash to post URL
    Thanks Yousuf Elkhoga for the bug report!
    
    This commit updates the `url` computed field in the `Post` document type
    within `contentlayer.config.js`. The `resolve` function now prepends a
    slash (`/`) to the flattened path of each post, ensuring that all URLs
    are absolute paths. This change ensures consistency and correctness in
    how URLs are generated for posts across the application.

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -209,7 +209,7 @@ export const Post = defineDocumentType(() => ({
   computedFields: {
     url: {
       type: "string",
-      resolve: (post) => post._raw.flattenedPath,
+      resolve: (post) => "/" + post._raw.flattenedPath,
     },
     timeToRead: {
       type: "number",
