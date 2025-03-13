653e5e429307bb31068cf3e6d1cbaa3eda069f21
Eliseo Martelli <me@eliseomartelli.it>
2025-03-13 10:27:23 +0100

    fix: escape special characters in HTML text

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,13 +7,13 @@ export default function ContactPage() {
     <article className="prose w-full mx-auto">
       <h1 className="font-serif">Get in touch</h1>
       <p>
-        I'd love to hear from you and learn more about how we can work together.
-        Let's start a conversation and see where it takes us!
+        I&apos;d love to hear from you and learn more about how we can work
+        together. Let&apos;s start a conversation and see where it takes us!
       </p>
       <p>
         Just fill out the contact form below or send me an{" "}
-        <Link href="mailto:info@eliseomartelli.it">email</Link>, and I'll get
-        back to you as soon as possible.
+        <Link href="mailto:info@eliseomartelli.it">email</Link>, and I&apos;ll
+        get back to you as soon as possible.
       </p>
       <ContactForm />
     </article>
