c89cdb9da45beabb31bfbe9beb1651d33676e608
Eliseo Martelli <me@eliseomartelli.it>
2025-03-13 11:29:40 +0100

    feat: add sitemap generation function

---
+++ src/app/sitemap.ts
@@ -0,0 +1,50 @@
+import { allTags } from "@/lib/tags";
+import { allNewsletters, allPhotos, allPosts } from "content-collections";
+
+export default async function sitemap() {
+  const blogs = allPosts.map((post) => ({
+    url: `https://eliseomartelli.it/blog/${post.slug}`,
+    lastModified: post.date,
+  }));
+
+  const photos = allPhotos.map((photo) => ({
+    url: `https://eliseomartelli.it/photos/${photo.slug}`,
+  }));
+
+  const tags = allTags.map((tag) => ({
+    url: `https://eliseomartelli.it/blog/tags/${tag}`,
+  }));
+
+  const newsletter = allNewsletters.map((newsletter) => ({
+    url: `https://eliseomartelli.it/newsletter/${newsletter.slug}`,
+  }));
+
+  const routes = [
+    "",
+    "/about",
+    "/blog",
+    "/contact",
+    "/feed.xml",
+    "/feedback",
+    "/photos",
+    "/recipes",
+    "/snippets",
+    "/ssh",
+    "/uses",
+    "/wallpapers",
+    "/newsletter",
+  ].map((route) => ({
+    url: `https://eliseomartelli.it${route}`,
+    lastModified: new Date().toISOString().split("T")[0],
+  }));
+
+  return [
+    ...routes,
+
+    // Dynamic
+    ...blogs,
+    ...photos,
+    ...tags,
+    ...newsletter,
+  ];
+}
