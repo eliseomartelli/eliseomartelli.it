560a68e33d41794d63352bf0f89b07558d80b1e2
Eliseo Martelli <me@eliseomartelli.it>
2025-03-13 11:30:25 +0100

    feat: add RSS feed generation

---
+++ src/app/feed.xml/route.tsx
@@ -0,0 +1,37 @@
+import { allSortedPosts } from "@/lib/sortedPosts";
+import { CustomMdx } from "@/components/mdx-custom/mdx";
+import RSS from "rss";
+
+export async function generateStaticParams() {
+  return [];
+}
+
+export async function GET() {
+  const feed = new RSS({
+    title: "Eliseo Martelli",
+    site_url: "https://eliseomartelli.it",
+    feed_url: "https://eliseomartelli.it/feed.xml",
+  });
+
+  const posts = allSortedPosts.slice(0, 5); // Return latest 5 articles.
+
+  const ReactDOMServer = (await import("react-dom/server")).default;
+
+  posts.map((post) => {
+    feed.item({
+      title: post.title,
+      url: `https://eliseomartelli.it/blog/${post.slug}`,
+      date: post.date,
+      description: ReactDOMServer.renderToStaticMarkup(
+        <CustomMdx code={post.mdx} />,
+      ),
+      categories: post.tags,
+    });
+  });
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      "Cache-Control": "public, s-maxage=1200, stale-while-revalidate=600",
+      "Content-Type": "text/xml",
+    },
+  });
+}
