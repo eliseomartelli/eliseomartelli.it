904a06e8fe54405f3f479b531ba2e98e31145193
Eliseo Martelli <me@eliseomartelli.it>
2025-03-13 11:01:37 +0100

    chore: update images config to use remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,38 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "eliseomartelli.it",
-      "www.eliseomartelli.it",
-      "localhost",
-      "files.mastodon.social",
-      "cdn.pony.social",
-      "raw.githubusercontent.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "eliseomartelli.it",
+        port: "",
+        pathname: "*",
+      },
+      {
+        protocol: "https",
+        hostname: "www.eliseomartelli.it",
+        port: "",
+        pathname: "*",
+      },
+      { protocol: "https", hostname: "localhost", port: "", pathname: "*" },
+      {
+        protocol: "https",
+        hostname: "files.mastodon.social",
+        port: "",
+        pathname: "*",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pony.social",
+        port: "",
+        pathname: "*",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "*",
+      },
     ],
   },
 };
