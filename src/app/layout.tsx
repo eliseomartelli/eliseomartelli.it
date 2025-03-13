ea7fa672ac4d1c5356816df9745eaf5e1e1ee8fe
Eliseo Martelli <me@eliseomartelli.it>
2025-03-13 12:39:21 +0100

    fix: remove unused font imports and variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toolbar } from "@/components/custom/toolbar";
 import { Footer } from "@/components/custom/footer";
 import { Toaster } from "sonner";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: {
@@ -30,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen w-full overflow-x-hidden`}
-      >
+      <body className="antialiased flex flex-col min-h-screen w-full overflow-x-hidden">
         <Toolbar />
         <main className="max-w-prose w-full mx-auto p-4 flex-1 space-y-6 mb-8">
           {children}
