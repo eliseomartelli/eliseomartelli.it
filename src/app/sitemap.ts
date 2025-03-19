0b6611edaca59d3879a8c0d65f35f3a4b4c88721
Eliseo Martelli <me@eliseomartelli.it>
2025-03-19 17:33:00 +0100

    feat: update sitemap function to return Promise<MetadataRoute.Sitemap>

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,23 +1,26 @@
 import { allTags } from "@/lib/tags";
 import { allNewsletters, allPhotos, allPosts } from "content-collections";
+import { MetadataRoute } from "next";
 
-export default async function sitemap() {
-  const blogs = allPosts.map((post) => ({
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const blogs: MetadataRoute.Sitemap = allPosts.map((post) => ({
     url: `https://eliseomartelli.it/blog/${post.slug}`,
     lastModified: post.date,
   }));
 
-  const photos = allPhotos.map((photo) => ({
+  const photos: MetadataRoute.Sitemap = allPhotos.map((photo) => ({
     url: `https://eliseomartelli.it/photos/${photo.slug}`,
   }));
 
-  const tags = allTags.map((tag) => ({
+  const tags: MetadataRoute.Sitemap = allTags.map((tag) => ({
     url: `https://eliseomartelli.it/blog/tags/${tag}`,
   }));
 
-  const newsletter = allNewsletters.map((newsletter) => ({
-    url: `https://eliseomartelli.it/newsletter/${newsletter.slug}`,
-  }));
+  const newsletter: MetadataRoute.Sitemap = allNewsletters.map(
+    (newsletter) => ({
+      url: `https://eliseomartelli.it/newsletter/${newsletter.slug}`,
+    }),
+  );
 
   const routes = [
     "/",
