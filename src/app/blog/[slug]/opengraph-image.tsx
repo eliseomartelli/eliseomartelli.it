66817660a1c933ddf91003857e940917ab0828a5
Eliseo Martelli <me@eliseomartelli.it>
2025-04-10 17:11:45 +0200

    feat: add background image to blog post opengraph-image

--- src/app/blog/[slug]/opengraph-image.tsx
+++ src/app/blog/[slug]/opengraph-image.tsx
@@ -22,19 +22,28 @@ export default async function Image({ params }: { params: { slug: string } }) {
     notFound();
   }
 
+  // https://stackoverflow.com/questions/1152024/best-way-to-generate-a-random-color-in-javascript
   const view = (
-    <div tw="flex flex-col w-full h-full p-24 bg-stone-100">
+    <div
+      tw="flex flex-col w-full h-full p-24 bg-stone-100 text-white"
+      style={{
+        background: `
+        linear-gradient(to bottom, rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
+        linear-gradient(to top left,${"#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6)},${"#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6)}),
+`,
+      }}
+    >
       <div tw="flex flex-col">
-        <p tw="mt-0 text-xl">{post.formattedDate}</p>
-        <h2 tw="text-6xl font-bold text-gray-900 text-left tracking-tight max-w-2/3">
+        <p tw="mt-0 text-2xl">{post.formattedDate}</p>
+        <h2 tw="text-6xl font-bold text-left tracking-tight max-w-2/3">
           {post.title}
         </h2>
-        <p tw="text-xl max-w-1/2">{post.excerpt}</p>
-        <div tw="flex">
+        <p tw="text-2xl max-w-1/2">{post.excerpt}</p>
+        <div tw="flex mt-8">
           {post.tags.map((tag, i) => (
             <span
               key={i}
-              tw={`${colorClassFromPostTag(tag)} py-2 px-4 rounded-full`}
+              tw={`${colorClassFromPostTag(tag)} py-2 px-4 rounded-full text-2xl mr-2`}
             >
               {tag}
             </span>
@@ -43,7 +52,7 @@ export default async function Image({ params }: { params: { slug: string } }) {
       </div>
       <div tw="flex grow"></div>
       <div tw="absolute flex flex-row items-center justify-end bottom-10 right-10">
-        <h1 tw="text-3xl">Eliseo Martelli</h1>
+        <h1 tw="text-3xl mr-4 font-bold">Eliseo Martelli</h1>
         <img
           src="https://eliseomartelli.it/icon.png"
           tw="w-24 ml-4"
