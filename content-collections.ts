a74e5d9646dfb07ddbaaf641bd022b79dfc7cd08
Eliseo Martelli <me@eliseomartelli.it>
2025-08-28 14:43:07 +0200

    feat: add camera resource

--- content-collections.ts
+++ content-collections.ts
@@ -1,9 +1,10 @@
 import { about } from "@/lib/about";
+import { cameras } from "@/lib/cameras";
 import { newsletter } from "@/lib/newsletter";
 import { photos } from "@/lib/photos";
 import { posts } from "@/lib/posts";
 import { defineConfig } from "@content-collections/core";
 
 export default defineConfig({
-  collections: [posts, about, photos, newsletter],
+  collections: [cameras, posts, about, photos, newsletter],
 });
