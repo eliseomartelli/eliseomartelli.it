a1cd9745586d17b9425a30152e192898a7e7d95e
Eliseo Martelli <me@eliseomartelli.it>
2025-09-06 11:04:45 +0200

    chore: change content collections

--- content-collections.ts
+++ content-collections.ts
@@ -1,10 +1,9 @@
 import { about } from "@/lib/about";
-import { cameras } from "@/lib/cameras";
 import { newsletter } from "@/lib/newsletter";
 import { photos } from "@/lib/photos";
 import { posts } from "@/lib/posts";
 import { defineConfig } from "@content-collections/core";
 
 export default defineConfig({
-  collections: [cameras, posts, about, photos, newsletter],
+  collections: [posts, about, photos, newsletter],
 });
